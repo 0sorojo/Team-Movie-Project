1cfc41838e3a28456abce0f788d4f23cff08582b
Ajla Velagic <ajvelagic@gmail.com>
2020-08-23 16:55:58 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { WatchListComponent } from './watch-list/watch-list.component';
+import { SearchComponent } from './search/search.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainPageComponent,
+    WatchListComponent,
+    SearchComponent,
+    NotFoundComponent,
+    MovieCardComponent
   ],
   imports: [
     BrowserModule,
